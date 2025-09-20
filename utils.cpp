3e4bd505d899565cdc62b7dd6dd708992aa6c68d
RyanKPosey <157773263+RyanKPosey@users.noreply.github.com>
2025-09-20 23:46:19 +0000

    Add utils.cpp/.h

---
+++ utils.cpp
@@ -0,0 +1,8 @@
+/*
+CS1B – G2: Serendipity
+  Partner A: Ryan Posey — role: driver (mainmenu.cpp)
+  Partner B: Ian Bagherzadeh — role: Inventory Manager (invMenu.cpp/.h)
+  Date: 9/23/2025
+  Purpose: Menu based inventory manager
+  Build:   g++ -std=c++20 -Werror mainmenu.cpp invmenu.cpp reports.cpp cashier.cpp -o serendipity.out
+*/
