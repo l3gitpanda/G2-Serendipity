5da97a38192f30ec5f7e90a0a92895028e8485e1
RyanKPosey <157773263+RyanKPosey@users.noreply.github.com>
2025-09-21 05:30:03 +0000

    Add book.cpp to build script

--- book.cpp
+++ book.cpp
@@ -4,7 +4,7 @@ CS1B – G2: Serendipity
   Partner B: Ian Bagherzadeh — role: Inventory Manager (invMenu.cpp/.h)
   Date: 9/23/2025
   Purpose: Menu based inventory manager
-  Build:   g++ -std=c++20 -Werror mainmenu.cpp utils.cpp invmenu.cpp reports.cpp cashier.cpp -o serendipity.out
+  Build:   g++ -std=c++20 -Werror mainmenu.cpp utils.cpp invmenu.cpp reports.cpp book.cpp cashier.cpp -o serendipity.out
 */
 #include <iostream>
 #include "book.h"
