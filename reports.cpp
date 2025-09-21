60db89f63d43bcae29dcaca6e1f4f2239afa8302
RyanKPosey <157773263+RyanKPosey@users.noreply.github.com>
2025-09-21 01:42:22 +0000

    Add reports() & printReportsMenu()

--- reports.cpp
+++ reports.cpp
@@ -9,8 +9,43 @@ CS1B – G2: Serendipity
 
 #include <iostream>
 #include "reports.h"
+#include <iomanip>
 
 using namespace std;
+
+void printReportsMenu() {
+    int optionNumberPadding = 12;
+    /*
+    Distance between option number containers and
+    there corresponding label
+    */
+    int optionLabelPadding = 10;
+    /*
+    Distance between option label containers and 
+    their corresponding label number
+    */
+
+    cout << right << setw(40) << "SERENDIPITY" << endl << 
+        right << setw(optionNumberPadding) << "1:" << 
+        right << setw(optionLabelPadding) << "View Listing" << endl <<
+        right << setw(optionNumberPadding) << "2:" << 
+        right << setw(optionLabelPadding) << "View Wholesale" << endl <<
+        right << setw(optionNumberPadding) << "3:" << 
+        right << setw(optionLabelPadding) << "View Retail" << endl <<
+        right << setw(optionNumberPadding) << "4:" << 
+        right << setw(optionLabelPadding) << "View quantity" << endl <<
+        right << setw(optionNumberPadding) << "5:" << 
+        right << setw(optionLabelPadding) << "View Cost" << endl <<
+        right << setw(optionNumberPadding) << "6:" << 
+        right << setw(optionLabelPadding) << "View Age" << endl <<
+        right << setw(optionNumberPadding) << "7:" << 
+        right << setw(optionLabelPadding) << "Go back to main menu" << endl;
+        
+}
+
+void reports() {
+
+}
 
 void repListing() {
     cout << "You've chosen a listing";
