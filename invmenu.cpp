aff91f577b804fe0d99cc54a1a67b267d1cff7f8
l3gitpanda <callofdutyguy4@gmail.com>
2025-10-12 13:04:33 -0700

    Small fix

--- invmenu.cpp
+++ invmenu.cpp
@@ -2,7 +2,7 @@
 CS1B – G2: Serendipity
   Partner A: Ryan Posey — role: driver (mainmenu.cpp)
   Partner B: Ian Bagherzadeh — role: Inventory Manager (invMenu.cpp/.h)
-  Date: 9/23/2025
+  Date: 10/10/2025
   Purpose: Menu based inventory manager
   Build:   g++ -std=c++20 -Werror mainmenu.cpp utils.cpp invmenu.cpp reports.cpp bookType.cpp cashier.cpp -o serendipity.out
 */
